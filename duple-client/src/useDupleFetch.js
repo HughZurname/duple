4ac3444c46f214fbb0e53fbda2a0ad6898eba77c
Craig de Gouveia <craig.degouveia@gmail.com>
2020-01-22 01:00:14 +0000

    missing file

---
+++ duple-client/src/useDupleFetch.js
@@ -0,0 +1,14 @@
+import { useFetch } from '@bjornagh/use-fetch'
+
+const useDupleFetch = ({ url, init = {}, ...rest }) => {
+    const clientId = window.localStorage.getItem('clientId')
+    const apiRoot = 'http://localhost:8080'
+    const finalUrl = `${apiRoot}${url}`
+    const finalHeaders = { ...init.headers }
+    finalHeaders['clientId'] = clientId
+    init.headers = finalHeaders
+
+    return useFetch({ url: finalUrl, init, ...rest })
+}
+
+export default useDupleFetch
